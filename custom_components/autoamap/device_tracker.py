d34a197fcc5137d5d2246b8937099c42f529462d
dscao <dscao.com@gmail.com>
2022-12-31 13:02:16 +0800

    Update device_tracker.py

--- custom_components/autoamap/device_tracker.py
+++ custom_components/autoamap/device_tracker.py
@@ -152,7 +152,6 @@ class autoamapEntity(TrackerEntity):
 
     async def async_update(self):
         """Update autoamap entity."""
-        _LOGGER.debug("device tracker_update: %s", self.coordinator.data["MESSAGE"]["HD_STATE_TIME"])
         _LOGGER.debug(datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo)
         await self.coordinator.async_request_refresh()
         if self._gps_conver == True:
